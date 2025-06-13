8f443d38c801aec104d199e383d85cb61b5adb57
sarath647 <sarathkumar12d@gmail.com>
2025-06-13 08:32:56 +0530

    Add files via upload

---
+++ Task018.java
@@ -0,0 +1,7 @@
+public class Task018 {
+    public static void main(String[] args) {
+        Task017 myObj = new Task017();
+        myObj.name = "Sarath";
+        System.out.println(myObj.name);
+    }
+}
---
+++ Task019.java
@@ -0,0 +1,7 @@
+public class Task019 {
+    public static void main(String[] args) {
+        Task017 myObj = new Task017();
+        myObj.setName("sarath");
+        System.out.println(myObj.getName());
+    }
+}
